6504364cc2f07def10b1692c432430b787215826
alexwang <alex@9gag.com>
2015-10-07 01:31:59 +0800

    update navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container">
     <p class="navbar-text navbar-right">
-    	<a href='/'>List</a> | <a href='/fetch'>Fetch</a> | <a href='/bookmarklet'>BookMarkLet</a>
+    	<a href='/'>List</a> | <a href='/fetch'>Fetch</a> | <a href='/bookmarklet'>BookMark</a>
     </p>
   </div>
 </nav>
