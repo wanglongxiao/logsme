087fe118e90af9ce38cb574e36b7bd4445bf732e
alexwang <alex@9gag.com>
2015-10-11 22:30:49 +0800

    composer required Laravel\Socialite

--- config/app.php
+++ config/app.php
@@ -144,6 +144,9 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+    	
+    	// for social login support
+    	//Laravel\Socialite\SocialiteServiceProvider::class,
 
     ],
 
@@ -193,6 +196,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+    	// for social login support
+    	//'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
