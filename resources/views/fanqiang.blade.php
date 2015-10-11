f361d2724af7235460705c0eff67634bbbb7c757
alexwang <alex@9gag.com>
2015-10-11 17:50:40 +0800

    add howtoplayvideo page

--- resources/views/fanqiang.blade.php
+++ resources/views/fanqiang.blade.php
@@ -9,14 +9,16 @@
  		
  		<div class="media">
 		  <div class="media-body">
-		  	<h3 class="media-heading">如播放“海外视频”遇到问题，请参照一下步骤</h3>	    
+		  	<h3 class="media-heading">如播放“海外视频”遇到问题，请参照如下步骤</h3>	    
 		    <hr>
-		    <p>由于 GFW 的限制，部分地区无法播放海外视频，请检（Fan）查（Qiang）您的网络。</p>
-		    <p>请选择下载下列软件，安装并启动后再尝试访问：</p>
+		    <p>由于 GFW 的限制，部分地区无法播放海外视频，播放前先检（Fan）测（Qiang）您的网络。</p>
+		    <p>请下载并安装相应软件，启动后再尝试访问：</p>
 		    <ul class="list-group">
-			  <li class="list-group-item">苹果手机</li>
-			  <li class="list-group-item">安卓手机</li>
-			  <li class="list-group-item">个人电脑</li>
+			  <li class="list-group-item"><a href="http://itunes.apple.com/cn/app/hola-privacy-vpn-app-browser/id903869356?mt=8"><b>[苹果手机]</b> 检(Fan)测(Qiang)</a></li>
+			  <li class="list-group-item"><a href="http://play.google.com/store/apps/details?id=org.hola&hl=zh-TW"><b>[安卓手机]</b> 检(Fan)测(Qiang)</a></li>
+			  <li class="list-group-item"><a href="http://chrome.google.com/webstore/detail/unlimited-free-vpn-hola/gkojfkhlekighikafcpjkiklfbnlmeio?hl=zh-TW"><b>[个人电脑]</b> 检(Fan)测(Qiang)</a></li>
+			  <li class="list-group-item"><a href="http://www.freefq.com">[中文] 检(Fan)测(Qiang)网站</a></li>
+			  <li class="list-group-item"><a href="https://hola.org/download">[英文] 检(Fan)测(Qiang)网站</a></li>
 			</ul>
 		  </div>
 		</div>
